e44cb95497bb17f03391de5c02ae3f526768ae4a
mikoroonii <mikoroonii@gmail.com>
2025-02-03 22:22:33 +1100

    test

--- crazy.gd
+++ crazy.gd
@@ -1,5 +1,7 @@
 extends Node
 
+@export var NutsAreItchy: bool = true;
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,4 +11,4 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-# penis 
+# penis
